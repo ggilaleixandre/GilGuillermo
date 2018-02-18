173321f50789756168c5257cf04dfef531fd01dd
Yo <Yo@DESKTOP-LAJF6KO>
2018-02-18 22:38:37 +0100

    Generanda Documentación

--- src/calculadora/RPN.java
+++ src/calculadora/RPN.java
@@ -1,54 +1,90 @@
 package calculadora;
 
+/**
+ *  Calculadora con notación polaca inversa.
+ * @author Guillermo
+ *
+ */
 
 public class RPN {
-	
+	/**
+	 * Inserta un dato en la pila.
+	 * @param nuevo_dato Dato que inserta en la pila.
+	 */
 	public void pushPila(double nuevo_dato) {
 		NodoPila nuevo_nodo = new NodoPila(nuevo_dato, arriba);
 		arriba = nuevo_nodo;
 	}
+	/**
+	 * Extrae un dato de la pila.
+	 * @return Dato que extrae de la pila.
+	 */
 	public double popPila( ) {
 		double dato_arriba = arriba.dato;
 		arriba = arriba.abajo;
 		return dato_arriba;
 	}
+	/**
+	 * Pone un comando a ejecutar por la calculadora.
+	 * @param commando Comando a ejecutar.
+	 */
 	public RPN(String commando) {
 		arriba = null;
 		this.commando = commando;
 	}
 	
+	/**
+	 * Ejercuta la operación de Suma
+	 */
 	public void suma() {
 		b = popPila( );
 		a = popPila( );
 		pushPila(a + b);
 	}
+	/**
+	 * Ejecuta la operación de Resta
+	 */
 	public void resta() {
 		b = popPila( );
 		a = popPila( );
 		pushPila(a - b);
 	}
+	/**
+	 * Ejecuta la operación de multiplicación.
+	 */
 	public void multiplicacion(){
 		b = popPila( );
 		a = popPila( );
 		pushPila(a * b);
 	}
+	/**
+	 * Ejecuta la operación de división.
+	 */
 	public void division() {
 		b = popPila( );
 		a = popPila( );
 		pushPila(a / b);
 	}
+	/**
+	 * Ejecuta la operación de exponencial.
+	 */
 	public void exponencial() {
 		b = popPila( );
 		a = popPila( );
 		pushPila(Math.pow(a, b));
 	}
-	
+	/**
+	 * Ejecuta la operación de resto.
+	 */
 	public void resto() {
 		b = popPila( );
 		a = popPila( );
 		pushPila(a%b);
 	}
-	
+	/**
+	 * Convierte un String en número y lo mete en la pila.
+	 * @param temp String a convertir a numero.
+	 */
 	public void obtenerNumero(String temp) {
 		
 		// convertir a double y añadir a la pila
@@ -57,7 +93,9 @@ public class RPN {
 		
 	}
 	
-	
+	/**
+	 * Ejecuta una de las posibles operaciones.
+	 */
 	public void operaciones() {
 		for(int i = 0; i < commando.length( ); i++) {
 			// si es un digito
@@ -84,7 +122,10 @@ public class RPN {
 			}
 		}
 	}
-	
+	/**
+	 * Devuelve el resultado de la operación.
+	 * @return Resultado de la operación.
+	 */
 	public double resultado( ) {
 
 		this.operaciones();
